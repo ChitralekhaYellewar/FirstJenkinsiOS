c219446b950224d8a013cdbacd30b093e8f12fcd
Chitralekha Yellewar <chitralekha.yellewar@hp.com>
2018-08-28 17:49:02 +0530

    changed code

---
+++ FirstJenkinsiOS/FirstJenkinsiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstJenkinsiOS
+//
+//  Created by Chitralekha Yellewar on 28/08/18.
+//  Copyright © 2018 Chitralekha Yellewar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstJenkinsiOS/FirstJenkinsiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstJenkinsiOS
+//
+//  Created by Chitralekha Yellewar on 28/08/18.
+//  Copyright © 2018 Chitralekha Yellewar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
